320bf996433cb1e18107a47b9687526d9ca9ace0
Furiml <a.screve.94801@gmail.com>
2018-06-18 16:30:08 +0200

    update & rockpaperscissors kata

--- duplicate_encoder.java
+++ duplicate_encoder.java
@@ -4,13 +4,13 @@
 
 public class DuplicateEncoder {
 	static String encode(String word){
-    word = word.toLowerCase(); //get rid of uppercase
+    word = word.toLowerCase(); // get rid of uppercase
     String[] splitword = word.split("");
     String newWord = "";
     for (int i=0; i<word.length(); i++) {
-      int occChecker = 0; //occChecker is an occurence counter
+      int occChecker = 0; // occChecker is an occurence counter
       for (int index = word.indexOf(splitword[i]); index >= 0; index = word.indexOf(splitword[i], index + 1))
-          occChecker++; //if there's more than 1 occurence then we put a )
+          occChecker++; // if there's more than 1 occurence then we put a )
       newWord += (occChecker > 1) ? ")" : "(";
     }
     return newWord;
